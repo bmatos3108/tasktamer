1513545bdb3a1b34e9c2e3d23cf258765542f514
Barbara Matos <bmatos3108@gmail.com>
2024-08-03 23:15:09 +0200

    improving sign in and sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  def set_sidebar_events
-    if user_signed_in?
-      @events = current_user.events
-    else
-      @events = []
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # Permit the `name` parameter for sign-up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # Permit the `name` parameter for account update
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
   end
-end
